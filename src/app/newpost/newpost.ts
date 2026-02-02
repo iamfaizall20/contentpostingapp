0be20359bc41f79b6ee1895536336f0b6b05c0a5
Faizal Hassan <hassanfaizal047@gmail.com>
2026-02-02 11:15:45 +0500

    implemented new post feature

--- src/app/newpost/newpost.ts
+++ src/app/newpost/newpost.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-newpost',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, FormsModule, RouterOutlet, RouterLink,HttpClientModule],
   templateUrl: './newpost.html',
   styleUrl: './newpost.css',
 })
 export class Newpost {
+
+  constructor(private http: HttpClient, private router: Router) { }
+  apiURL = "http://localhost/contentpostingappapis/posts/create.php";
+
   tags: string[] = [];
   coverPreview: string | null = null;
+  coverFile: any;
 
   addTag(event: any) {
     event.preventDefault();
@@ -31,6 +37,8 @@ export class Newpost {
     const file = event.target.files[0];
     if (!file) return;
 
+    this.coverFile = file;
+
     const reader = new FileReader();
     reader.onload = () => {
       this.coverPreview = reader.result as string;
@@ -46,32 +54,35 @@ export class Newpost {
   suggestTagsAI() { };
   generateCoverAI() { };
 
-
   publishPost(form: any) {
-    if (form.invalid) {
-      alert("Please fill the content");
+    if (!form.value.title || !form.value.content) {
+      alert("Please fill all fields");
       return;
     }
-
-    // Form Data to generate json
+  
     const formData = new FormData();
-
     const user = JSON.parse(localStorage.getItem('user')!);
-
-    formData.append('userId', user.userId);
+  
+    formData.append('userId', user.userId); 
     formData.append('title', form.value.title);
     formData.append('content', form.value.content);
-
-
-    this.tags.forEach((tag) => {
+  
+    this.tags.forEach(tag => {
       formData.append('tags[]', tag);
     });
-
-    if (this.coverPreview) {
-      formData.append('cover_image', this.coverPreview);
+  
+    if (this.coverFile) {
+      formData.append('cover_image', this.coverFile);
     }
-
-    console.log(formData);
-
-  }
+  
+    this.http.post(this.apiURL, formData).subscribe({
+      next: (res: any) => {
+        alert("Post Added Successfully");
+        this.router.navigate(['/']);
+      },
+      error: err => {
+        console.log('Error', err);
+      }
+    });
+  }  
 }

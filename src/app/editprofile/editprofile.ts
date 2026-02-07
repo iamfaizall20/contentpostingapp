82adc27657c170aee6b9fff37129f9047b171cd8
Faizal Hassan <hassanfaizal047@gmail.com>
2026-02-07 13:38:05 +0500

    ---

--- src/app/editprofile/editprofile.ts
+++ src/app/editprofile/editprofile.ts
@@ -3,19 +3,24 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Navbar } from '../navbar/navbar';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-editprofile',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink, Navbar],
+  imports: [CommonModule, FormsModule, RouterLink, Navbar, HttpClientModule],
   templateUrl: './editprofile.html',
   styleUrl: './editprofile.css',
 })
 export class Editprofile {
 
+  constructor(private http: HttpClient) { }
+  username: string = '';
+  newpassword: string = '';
+  oldpassword: string = '';
+
   activeTab: 'profile' | 'account' | 'security' = 'profile';
 
-  // Dummy user (replace with API later)
   user = {
     name: 'John Doe',
     email: 'john@example.com',
@@ -24,5 +29,54 @@ export class Editprofile {
 
   setTab(tab: 'profile' | 'account' | 'security') {
     this.activeTab = tab;
+  }
+
+  changeUsername() {
+    const user = JSON.parse(localStorage.getItem('user')!);
+
+    const formData = new FormData();
+
+    formData.append('userId', user.userId);
+    formData.append('username', this.username);
+
+    this.http.post('http://localhost/contentpostingappapis/editprofile/write.php', formData).subscribe({
+      next: (res: any) => {
+        if (res) {
+          alert(res.message);
+        }
+      },
+      error: (err: any) => {
+        alert(err.message);
+      }
+    })
+
+  }
+  changePassword() {
+    const user = JSON.parse(localStorage.getItem('user')!);
+
+    const formData = new FormData();
+
+    formData.append('userId', user.userId);
+    formData.append('username', this.username);
+    formData.append('password', this.newpassword);
+    formData.append('oldpassword', this.oldpassword);
+
+    this.http.post('http://localhost/contentpostingappapis/editprofile/write.php', formData).subscribe({
+      next: (res: any) => {
+        if (res) {
+          alert(res.message);
+        }
+      },
+      error: (err: any) => {
+        alert(err.message);
+      }
+    })
+
+  }
+
+  saveChanges() {
+    if (this.activeTab === 'profile') {
+      this.changeUsername();
+    }
   }
 }
